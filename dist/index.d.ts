aebb8b5721d8ae1a8cb2c446cc739e22da186b4e
Karthik R <karthikeya.rachamolla@gmail.com>
2025-08-13 22:30:12 -0400

    added tslib?

---
+++ dist/index.d.ts
@@ -0,0 +1,11 @@
+import * as react_jsx_runtime from 'react/jsx-runtime';
+import { ReactNode } from 'react';
+
+type PageProps = {
+    title: string;
+    children: ReactNode;
+};
+
+declare const Page: ({ title, children }: PageProps) => react_jsx_runtime.JSX.Element;
+
+export { Page };
